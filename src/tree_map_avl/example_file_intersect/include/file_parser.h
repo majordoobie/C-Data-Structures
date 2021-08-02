cc319a1be7adee450747945556542d1f7965b4e5
Jay Galindez <sgtmajordoobie@live.com>
2021-08-02 11:56:43 -0400

    [Added insertion example] Example on how to use the tree map

---
+++ src/tree_map_avl/example_file_intersect/include/file_parser.h
@@ -0,0 +1,7 @@
+#ifndef FILE_PARSER_H
+#define FILE_PARSER_H
+#include <arg_parser.h>
+
+void read_file(file_tracker_t * tracker);
+
+#endif //FILE_PARSER_H
---
+++ src/tree_map_avl/example_file_intersect/test/bst_test_functions.h
@@ -0,0 +1,30 @@
+#ifndef BST_TEST_FUNCTIONS_H
+#define BST_TEST_FUNCTIONS_H
+#include <check.h>
+#include <stdio.h>
+#include <bst.h>
+#include <stdlib.h>
+
+typedef struct bst_node_payload_t {
+    int value;
+    int other_value;
+} my_structure;
+
+typedef struct {
+    my_structure ** my_struct_list;
+    int count;
+} iter_struct_t;
+
+typedef struct {
+    int target;
+    my_structure * payload;
+} recursion_test_t;
+
+bst_compare_t compare(my_structure * new_payload, my_structure * current_payload);
+void free_payload(my_structure * node_payload);
+my_structure * create_payload(int val1, int val2);
+
+void print(my_structure * node_payload);
+bst_recurse_t save_nodes(my_structure * node_payload, void * ptr);
+
+#endif //BST_TEST_FUNCTIONS_H
