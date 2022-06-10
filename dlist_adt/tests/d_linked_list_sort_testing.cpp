13c56b5a2a059d2145aa0ae7f67dd895d1b14eff
Jay Galindez <sgtmajordoobie@live.com>
2022-06-10 16:03:20 -0400

    [Sort Complete] Added two more test cases since I did not test for negative values nor duplicates which should not have been a problem but wanted to be sure.

--- dlist_adt/tests/d_linked_list_sort_testing.cpp
+++ dlist_adt/tests/d_linked_list_sort_testing.cpp
@@ -37,22 +37,30 @@ class DListTestSortFixture : public ::testing::Test
 {
  public:
     dlist_t * dlist{};
+    dlist_t * dlist2{};
+
     int length = 10;
-//    std::vector<int> test_array = {5, 3, 4, 6, 19, 1, -1, 8, 3, 19};
+    std::vector<int> test_array2 = {5, 3, 4, 6, 19, 1, -1, 8, 3, 19};
     std::vector<int> test_array = {2, 8, 7, 1, 3, 5, 6, 4};
 
  protected:
     void SetUp() override
     {
         dlist = dlist_init(match_int_payloads);
+        dlist2 = dlist_init(match_int_payloads);
         for (auto& v: test_array)
         {
             dlist_append(dlist, get_int_payload(v));
+        }
+        for (auto& v: test_array2)
+        {
+            dlist_append(dlist2, get_int_payload(v));
         }
     }
     void TearDown() override
     {
         dlist_destroy_free(dlist, free_int_payload);
+        dlist_destroy_free(dlist2, free_int_payload);
     }
 };
 
@@ -80,6 +88,28 @@ TEST_F(DListTestSortFixture, SortAscending)
     dlist_destroy_iter(iter);
 }
 
+// Test the ability to quick sort the double linked list
+TEST_F(DListTestSortFixture, SortAscendingAdv)
+{
+    // sort the test array
+    std::sort(this->test_array2.begin(), this->test_array2.end());
+
+    dlist_quick_sort(this->dlist2, ASCENDING, compare_int_payloads);
+    dlist_iter_t * iter = dlist_get_iterable(dlist2, ITER_HEAD);
+
+    int count = 0;
+    int node = *(int *)dlist_get_iter_value(iter);
+
+    while (count < (int)this->test_array2.size() - 1)
+    {
+        const int& target = this->test_array2.at(count);
+        EXPECT_EQ(node, target);
+        node = *(int*)dlist_get_iter_next(iter);
+        count++;
+    }
+
+    dlist_destroy_iter(iter);
+}
 
 // Test ability to quick sort the double linked list
 TEST_F(DListTestSortFixture, SortDescending)
@@ -97,6 +127,31 @@ TEST_F(DListTestSortFixture, SortDescending)
     while (count < (int)this->test_array.size() - 1)
     {
         const int& target = this->test_array.at(count);
+        EXPECT_EQ(node, target);
+        node = *(int*)dlist_get_iter_next(iter);
+        count++;
+    }
+
+    dlist_destroy_iter(iter);
+}
+
+// Test ability to quick sort the double linked list
+TEST_F(DListTestSortFixture, SortDescendingAdv)
+{
+    // sort the test array
+    std::sort(this->test_array2.begin(), this->test_array2.end());
+    sort(this->test_array2.begin(), this->test_array2.end(), std::greater<int>
+        ());
+
+    dlist_quick_sort(this->dlist2, DESCENDING, compare_int_payloads);
+    dlist_iter_t * iter = dlist_get_iterable(dlist2, ITER_HEAD);
+
+    int count = 0;
+    int node = *(int *)dlist_get_iter_value(iter);
+
+    while (count < (int)this->test_array2.size() - 1)
+    {
+        const int& target = this->test_array2.at(count);
         EXPECT_EQ(node, target);
         node = *(int*)dlist_get_iter_next(iter);
         count++;
