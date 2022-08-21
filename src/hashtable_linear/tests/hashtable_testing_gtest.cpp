02bf723e11f18a93f84c070e0f2abb698b2f46ab
doobie <sgtmajordoobie@live.com>
2022-08-21 19:49:21 -0400

    [Implemented looksup] Going to now implement the deletion that python uses

--- src/hashtable_linear/tests/hashtable_testing_gtest.cpp
+++ src/hashtable_linear/tests/hashtable_testing_gtest.cpp
@@ -54,10 +54,26 @@ TEST_F(HashtableGtest, TestAllocAndDestroy)
 }
 
 // Test that we can check that a key exists in the hashtable already
+// Then test that we can safely fetch for a key that does not exist
 TEST_F(HashtableGtest, TestKeyExists)
 {
     EXPECT_EQ(true, hashtable_key_exists(this->dict, this->keys.at(0).c_str()));
     EXPECT_EQ(false, hashtable_key_exists(this->dict, "UnknownKey"));
+    EXPECT_EQ(nullptr, hashtable_get(this->dict, "Unknownkey"));
+}
+
+// Test that updating a key will yield the value that was stored so that
+// we can free it
+TEST_F(HashtableGtest, TestValueFreeOnUpdate)
+{
+    std::string first_key = this->keys.at(0);
+    char * new_payload = get_payload(first_key.c_str());
+    EXPECT_EQ(true, hashtable_key_exists(this->dict, this->keys.at(0).c_str()));
+
+    void * old_payload = hashtable_set(this->dict, first_key.c_str(), new_payload);
+
+    EXPECT_EQ(true, hashtable_key_exists(this->dict, this->keys.at(0).c_str()));
+    free(old_payload);
 }
 
 
